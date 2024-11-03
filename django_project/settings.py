4a406b1b773dc9411074e77333e2b1d2fcb75e84
Shahadat Hossain <sht.kcn@gmail.com>
2024-11-04 03:31:45 +0600

    Final touch for vercel deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p964a02+f8-4*#z$eyz^kjr8+zfi^+mwi=6ab+vay%4x6e*wf&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
